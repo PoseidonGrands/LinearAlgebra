7324ad65e1717e3797a26014659a4a8bb6c684fd
PoseidonGrands <sewellhe@outlook.com>
2023-10-06 13:47:10 +0800

    1、实现计算向量的模和向量归一化 2、实现计算向量点乘

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ if __name__ == "__main__":
     v_zero = Vector.zero(2)
     print(f"{v1} + {v_zero} = {v1 + v_zero}")
 
+    v3 = Vector([5, 2])
+    print(f"norm{v3} = {v3.norm()}")
+    print(f"norm{v_zero} = {v_zero.norm()}")
+
+    v4 = Vector([5, 2])
+    print(f"normalize{v4} = {v4.normalize()}")
+    # 验证有效性：是否真的是单位向量，长度为1
+    print(v4.normalize().norm())
+    print(v2.normalize().norm())
+
+    # print(v_zero.normalize())
+
+    print(v1.dot(v2))
+    print(v1 * v2)
 
 
 
--- playLA/Vector.py
+++ playLA/Vector.py
@@ -1,3 +1,7 @@
+import math
+from ._global import EPSILON
+
+
 class Vector:
     def __init__(self, ls):
         # 复制一份防止引用的外部列表修改导致向量被修改
@@ -24,11 +28,23 @@ class Vector:
 
         return Vector([(v_i - u_i) for v_i, u_i in zip(self, vec2)])
 
+    def dot(self, vec2):
+        """向量的点乘：两个向量相乘"""
+
+        assert len(self) == len(vec2),\
+            "the vector lenght must be same..."
+
+        return sum((x_i * y_i) for x_i, y_i in zip(self, vec2))
+
     def __mul__(self, k):
         return Vector([k * v_i for v_i in self])
 
     def __rmul__(self, k):
         return self * k
+
+    def __truediv__(self, k):
+        """向量除法：self / k"""
+        return self * (1 / k)
 
     def __pos__(self):
         return self * 1
@@ -39,6 +55,23 @@ class Vector:
     @classmethod
     def zero(cls, dimension):
         return Vector(dimension * [0])
+
+    def norm(self):
+        """求向量的模"""
+        return math.sqrt(sum(e**2 for e in self))
+
+    def normalize(self):
+        """向量归一化"""
+        # 分母不能为0，防止浮点数精度问题，采用EPSILON
+        if self.norm() < EPSILON:
+            raise ZeroDivisionError("normalize error...the vector nor is zero...")
+
+        # 防止引用问题：乘self相当于拷贝了一份
+        # 不能使用self / self.norm的原因：此前向量类只定义了乘法
+        # return 1 / self.norm() * self
+
+        # 定义除法操作后
+        return self / self.norm()
 
     def __iter__(self):
         return self._values.__iter__()
